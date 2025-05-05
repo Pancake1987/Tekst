7aff8d37e01abcc7ca35449ed87acf325c51671c
Pancake1987 <255424@edu.p.lodz.pl>
2025-05-05 12:45:44 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@ char *str2 = " ma kota. ";
 char *str3 = "";
 
 int combine(char *result) {
-    // Najpierw kopiujesz pierwszy łańcuch
+    
     strcpy(result, str1);
 
-    // Potem dołączasz drugi i trzeci
+    
     strcat(result, str2);
     strcat(result, str3);
 
@@ -17,12 +17,12 @@ int combine(char *result) {
 }
 
 int main() {
-    char result[100];  // Zmienna na wynik połączenia
+    char result[100];  
 
-    // Łączenie łańcuchów
+    
     combine(result);
 
-    // Wyświetlanie połączonego wyniku
+    
     printf("Polaczony tekst: %s\n", result);
 
     return 0;
